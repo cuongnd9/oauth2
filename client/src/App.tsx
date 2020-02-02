764d16acf17c9ceaf76d7fcabb325bdd1e84f758
Nguyen Duy Cuong <cuongw.me@gmail.com>
2020-02-02 22:28:05 +0700

    Add Facebook dialog for client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Login';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' render={() => <h1>Home</h1>} />
+        <Route path='/auth'>
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
